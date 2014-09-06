18a8e0f72d74cbbc4e487d852ae5be392f8569df
Daria_Serkova <serkova-darya@mail.ru>
2014-09-07 00:06:03 +0400

    First test program. Example from https://mahout.apache.org/users/recommender/userbased-5-minutes.html

---
+++ mahout/src/main/java/com/daria/edu/machinelearning/mahout/EvaluateRecommender.java
@@ -0,0 +1,23 @@
+package com.daria.edu.machinelearning.mahout;
+
+import java.io.File;
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.model.DataModel;
+
+/**
+ * Test program Example from
+ * https://mahout.apache.org/users/recommender/userbased-5-minutes.html
+ */
+public class EvaluateRecommender {
+
+	public static void main(String[] args) throws Exception {
+		DataModel model = new FileDataModel(new File("data/dataset.csv"));
+		RecommenderEvaluator evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
+		RecommenderBuilder builder = new MyRecommenderBuilder();
+		double result = evaluator.evaluate(builder, null, model, 0.9, 1.0);
+		System.out.println(result);
+	}
+}
